40b17d14bcd4723f0f8710f0319a7b81eb10257c
Andrew <ajzno26@gmail.com>
2023-06-21 11:02:52 -0400

    Added data model and helper functions & cleaned up code

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware; 
+from fastapi.middleware.cors import CORSMiddleware
 import pandas as pd
 from typing import Dict, List, Any
 import numpy as np
 import plotly.graph_objs as go
 import plotly.express as px
 import json
+from data_model import data_model
 
 app = FastAPI()
 
@@ -18,139 +19,143 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/fetch_and_clean_data/")
-async def fetch_and_clean_data():
+@app.on_event("startup")
+async def load_data():
+    global data_frame
     try:
         # Load the data from a CSV file
-        global data_frame
         data_frame = pd.read_csv('data_demo_2.csv', index_col=0)
-        
-         # Convert dates to datetime format
-        data_frame["Date of birth"] = pd.to_datetime(data_frame["Date of birth"], format='%Y-%m-%d')
-        data_frame["Date of diagnosis"] = pd.to_datetime(data_frame["Date of diagnosis"], format='%Y-%m-%d')
-        data_frame["Date of death"] = pd.to_datetime(data_frame["Date of death"], format='%Y-%m-%d')
-
-        # Calculate ages and survival time
-        data_frame["Age at diagnosis in years"] = (data_frame["Date of diagnosis"] - data_frame[
-            "Date of birth"]) / np.timedelta64(1, 'Y')
-        data_frame["Age at death in years"] = (data_frame["Date of death"] - data_frame["Date of birth"]) / np.timedelta64(
-            1, 'Y')
-        data_frame["Survival time in years"] = (data_frame["Date of death"] - data_frame[
-            "Date of diagnosis"]) / np.timedelta64(1, 'Y')
-        
-        # Round ages and survival time to 2 decimal places
-        data_frame["Age at diagnosis in years"] = data_frame["Age at diagnosis in years"].round(2)
-        data_frame["Age at death in years"] = data_frame["Age at death in years"].round(2)
-        data_frame["Survival time in years"] = data_frame["Survival time in years"].round(2)
-        return data_frame
-    
+        preprocess_data()
     except FileNotFoundError:
-        return {"error": "Data file not found."}
+        raise Exception("Data file not found")
     except pd.errors.ParserError:
-        return {"error": "Error parsing the data file."}
+        raise Exception("Error parsing the data file")
 
+# Transform the raw data
+def preprocess_data():
+    date_columns, age_columns, date_pairs = get_column_names()
 
-@app.get("/stats")
-# Return a hard-coded dictionary of statistics
-async def get_stats() -> Dict[str, int]:
-    stats = {
-        "Patients": 15000,
-        "Hospitals": 8,
-        "Researchers": 20,
-        "Queries": 1537,
-        "Compute (hrs)": 60     
-    }
-    return stats
+    # Convert dates to datetime format and calculate ages
+    for column, (start_date, end_date) in zip(age_columns, date_pairs):
+        data_frame[start_date] = pd.to_datetime(data_frame[start_date], format='%Y-%m-%d')
+        data_frame[end_date] = pd.to_datetime(data_frame[end_date], format='%Y-%m-%d')
+        data_frame[column] = ((data_frame[end_date] - data_frame[start_date]) / np.timedelta64(1, 'Y')).round(2)
 
-@app.get("/graphs")
-async def get_graph_data():
-    # Fetch and clean the data
-    data_frame = await fetch_and_clean_data()
-
-    # Define the categories of attributes
-    categorical_attributes = ["Sex", "Rurality", "Race", 'Socioeconomic', 'Ethnicity']
+# Return categorical and continuous attributes from the data model
+def get_attributes_by_type():
+    categorical_attributes = [value['series_name'] for key, value in data_model.items() if value['__type__'] == 'SeriesDataModelCategorical']
+    # continuous_attributes = [value['series_name'] for key, value in data_model.items() if value['__type__'] == 'SeriesDataModelDate']
+    # continuous_attributes.extend(["Age at diagnosis in years", "Age at death in years", "Survival time in years"])
     continuous_attributes = ["Age at diagnosis in years", "Age at death in years", "Survival time in years"]
+    return categorical_attributes, continuous_attributes
+
+# Return column names used in data preprocessing
+def get_column_names():
+    date_columns = ["Date of birth", "Date of diagnosis", "Date of death"]
+    age_columns = ["Age at diagnosis in years", "Age at death in years", "Survival time in years"]
+    date_pairs = [("Date of birth", "Date of diagnosis"), ("Date of birth", "Date of death"), ("Date of diagnosis", "Date of death")]
+    return date_columns, age_columns, date_pairs
+
+# Return the cleaned dataframe
+@app.get("/fetch_and_clean_data/")
+async def fetch_and_clean_data() -> Any:
+    return data_frame
+
+# Return a hard-coded dictionary of statistics
+@app.get("/stats")
+async def get_stats() -> Dict[str, int]:
+    return {"Patients": 15000, "Hospitals": 8, "Researchers": 20, "Queries": 1537, "Compute (hrs)": 60}
+
+# Generate pie charts and histograms from the data
+@app.get("/graphs")
+async def get_graph_data() -> Dict[str, Any]:
+    # Define the categories of attributes
+    categorical_attributes, continuous_attributes = get_attributes_by_type()
+
+    pie_charts, histograms = {}, {}
+    pie_charts.update(generate_pie_charts(categorical_attributes))
+    histograms.update(generate_histograms(continuous_attributes))
     
-    # Initialize empty dictionaries to store pie charts and histograms
+    return {"pie_charts": pie_charts, "histograms": histograms}
+
+# Generate pie charts for each categorical attribute
+def generate_pie_charts(categorical_attributes):
     pie_charts = {}
-    histograms = {}
-    
-    # Generate pie charts for each categorical attribute
     for column in categorical_attributes:
         if column in data_frame.columns:
             val_counts = data_frame[column].value_counts()
             val_counts_df = pd.DataFrame({'attribute': val_counts.index, 'value': val_counts.values})
 
-            # Convert pie chart object into a JSON format string
             pie_chart = px.pie(val_counts_df, values='value', names='attribute', title='').to_json()
-
-            # Convert the JSON string into a Python object (dict)
             pie_charts[column] = json.loads(pie_chart)
+    return pie_charts
 
-    # Generate histograms for each continuous attribute
+# Generates histograms for each continuous attribute
+def generate_histograms(continuous_attributes):
+    histograms = {}
     for column in continuous_attributes:
         if column in data_frame.columns:
             hist = px.histogram(data_frame, x=column, nbins=50, title='', labels={'value': 'Count', column: column}).to_json()
             histograms[column] = json.loads(hist)
+    return histograms
+
+# Generate comparison visualizations for the chosen hospitals and attributes
+@app.get("/compare/")
+async def compare_metrics(chosen_hospitals: List[str], attributes_to_be_compared: List[str]) -> Dict[str, Any]:
+    validate_inputs(chosen_hospitals, attributes_to_be_compared)
+
+    categorical_attributes, continuous_attributes = get_attributes_by_type()
+
+    pie_charts, histograms = {}, {}
+    pie_charts.update(generate_comparison_pie_charts(chosen_hospitals, attributes_to_be_compared, categorical_attributes))
+    histograms.update(generate_comparison_histograms(chosen_hospitals, attributes_to_be_compared, continuous_attributes))
 
     return {"pie_charts": pie_charts, "histograms": histograms}
 
-@app.get("/compare/")
-async def compare_metrics(chosen_hospitals: List[str], attributes_to_be_compared: List[str]) -> Dict[str, Any]:
-    try:
-        # Ensure at least two and at most three hospitals are chosen for comparison
-        if len(chosen_hospitals) < 2:
-            return {"error": "You need to select at least two hospitals for comparison."}
-        if len(chosen_hospitals) > 3:
-            return {"error": "You can select a maximum of three hospitals for comparison."}
-        
-        # Ensure at least one attribute is chosen for comparison
-        if len(attributes_to_be_compared) == 0:
-            return {"error": "You need to select at least one attribute for comparison."}
-        
-        # Fetch and clean the data
-        data_frame = await fetch_and_clean_data()
+# Validate the chosen hospitals and attributes for comparison
+def validate_inputs(chosen_hospitals, attributes_to_be_compared):
+    if len(chosen_hospitals) < 2 or len(chosen_hospitals) > 3:
+        raise Exception("You need to select at least two and maximum three hospitals for comparison.")
+    
+    if len(attributes_to_be_compared) == 0:
+        raise Exception("You need to select at least one attribute for comparison.")
 
-        # Check if the chosen hospitals are available in the dataframe
-        available_hospitals = data_frame['Hospital name'].unique().tolist()
-        for hospital in chosen_hospitals:
-            if hospital not in available_hospitals:
-                return {"error": f"The hospital '{hospital}' is not available."}
+    # Check if the chosen hospitals are available in the dataframe
+    available_hospitals = data_frame['Hospital name'].unique().tolist()
+    for hospital in chosen_hospitals:
+        if hospital not in available_hospitals:
+            raise Exception(f"The hospital '{hospital}' is not available.")
 
-        # Check if the chosen attributes are available in the dataframe
-        available_attributes = data_frame.columns.tolist()
-        for attribute in attributes_to_be_compared:
-            if attribute not in available_attributes:
-                return {"error": f"The attribute '{attribute}' is not available."}
+    # Check if the chosen attributes are available in the dataframe
+    available_attributes = data_frame.columns.tolist()
+    for attribute in attributes_to_be_compared:
+        if attribute not in available_attributes:
+            raise Exception(f"The attribute '{attribute}' is not available.")
 
-        # Define the categories of attributes
-        categorical_attributes = ["Sex", "Rurality", "Race", 'Socioeconomic', 'Ethnicity']
-        continuous_attributes = ["Age at diagnosis in years", "Age at death in years", "Survival time in years"]
+# Generates pie charts for categorical attributes
+def generate_comparison_pie_charts(chosen_hospitals, attributes_to_be_compared, categorical_attributes):
+    pie_charts = {}
+    for attribute in attributes_to_be_compared:
+        if attribute in categorical_attributes:
+            for hospital in chosen_hospitals:
+                hospital_df = data_frame[data_frame['Hospital name'] == hospital]
+                val_counts = hospital_df[attribute].value_counts()
+                val_counts_df = pd.DataFrame({'attribute': val_counts.index, 'value': val_counts.values})
+                pie_chart = px.pie(val_counts_df, values='value', names='attribute', title=f'{hospital} - {attribute}').to_json()
+                pie_charts[f'{hospital}_{attribute}'] = json.loads(pie_chart)
+    return pie_charts
 
-        # Initialize empty dictionaries to store pie charts and histograms
-        pie_charts = {}
-        histograms = {}
-
-        # Generate pie charts for categorical attributes and histograms for continuous attributes
-        for attribute in attributes_to_be_compared:
-            if attribute in categorical_attributes:
-                for hospital in chosen_hospitals:
-                    hospital_df = data_frame[data_frame['Hospital name'] == hospital]
-                    val_counts = hospital_df[attribute].value_counts()
-                    val_counts_df = pd.DataFrame({'attribute': val_counts.index, 'value': val_counts.values})
-                    pie_chart = px.pie(val_counts_df, values='value', names='attribute', title=f'{hospital} - {attribute}').to_json()
-                    pie_charts[f'{hospital}_{attribute}'] = json.loads(pie_chart)
-            elif attribute in continuous_attributes:
-                data = []
-                shared_bins = np.histogram_bin_edges(data_frame[data_frame['Hospital name'].isin(chosen_hospitals)][attribute], bins=50)
-                for hospital in chosen_hospitals:
-                    hist, _ = np.histogram(data_frame[data_frame['Hospital name'] == hospital][attribute], bins=shared_bins)
-                    
-                    # Create a histogram for each hospital
-                    data.append(go.Histogram(x=shared_bins[:-1], y=hist, name=hospital, histnorm='percent', opacity=0.75))
-                layout = go.Layout(title=f'{attribute} comparison', barmode='stack', bargap=0.1, xaxis=dict(title=attribute, range=[0, 'auto']), yaxis=dict(title='Population Count'))
-                fig = go.Figure(data=data, layout=layout)
-                histograms[f'{attribute}_comparison'] = json.loads(fig.to_json())
-        return {"pie_charts": pie_charts, "histograms": histograms}
-    except Exception as e:
-        return {"error": str(e)}
+# Generates histograms for continuous attributes
+def generate_comparison_histograms(chosen_hospitals, attributes_to_be_compared, continuous_attributes):
+    histograms = {}
+    for attribute in attributes_to_be_compared:
+        if attribute in continuous_attributes:
+            data = []
+            shared_bins = np.histogram_bin_edges(data_frame[data_frame['Hospital name'].isin(chosen_hospitals)][attribute], bins=50)
+            for hospital in chosen_hospitals:
+                hist, _ = np.histogram(data_frame[data_frame['Hospital name'] == hospital][attribute], bins=shared_bins)
+                data.append(go.Histogram(x=shared_bins[:-1], y=hist, name=hospital, histnorm='percent', opacity=0.75))
+            layout = go.Layout(title=f'{attribute} comparison', barmode='stack', bargap=0.1, xaxis=dict(title=attribute, range=[0, 'auto']), yaxis=dict(title='Population Count'))
+            fig = go.Figure(data=data, layout=layout)
+            histograms[f'{attribute}_comparison'] = json.loads(fig.to_json())
+    return histograms
